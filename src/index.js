59a83bdc3dc9bbcdc4139aece262bec5665f3c64
shikilnar <shikilnar@ya.ru>
2022-01-30 07:51:42 +0500

    init: first option

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if(!matrix){
+        return [];
+    }
+
+    let res = matrix.reduce((prev, cur, i) => {
+        if (cur.constructor.name == "Array"){
+            cur = i % 2 ? cur.reverse() : cur;
+        }
+        prev.push(...cur);
+        return prev;
+    }, []);
+    
+    return res;
 }
